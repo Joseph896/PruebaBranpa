e16051ca3f3b4f2d85b734e09a83f8fd00801968
José Ramos <josecito598@hotmail.com>
2020-11-04 23:11:23 -0300

    Agregar archivos de proyecto.

---
+++ WsInicio/Controllers/OperacionesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WsInicio.Controllers
+{
+    public class OperacionesController : ApiController
+    {
+        [HttpGet]
+        [ActionName("Sumar")]
+        public int OpSumar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        [HttpGet]
+        [ActionName("Restar")]
+        public int OpRestar(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
---
+++ WsInicio/Controllers/PruebaController.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WsInicio.Controllers
+{
+    public class PruebaController : ApiController
+    {
+        [HttpGet]
+        [ActionName("GetSaludoWs")]
+        public string getSaludo(string nombre, string apellido)
+        {
+            return "Hola "+ nombre + "," + apellido;
+        }
+
+        [HttpPost]
+        [ActionName("GuardarNombre")]
+        public void GuardarNombre(string nombre)
+        {
+            
+        }
+    }
+}
